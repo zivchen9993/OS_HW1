c5a3f080ee133a0a3933a95228ffaf4f0619eaf8
zivchen9993 <zivchen9993@gmail.com>
2021-04-13 16:46:45 +0300

    first upload - not compiling

---
+++ cmake-build-debug/CMakeFiles/smash.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smash.dir/commands.cpp.o"
+  "CMakeFiles/smash.dir/signals.cpp.o"
+  "CMakeFiles/smash.dir/smash.cpp.o"
+  "smash"
+  "smash.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/smash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
